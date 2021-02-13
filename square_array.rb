5ee27edb2d520f9e34bbaee7427cff2d0780d7f1
Bianca <biancancanal@gmail.com>
2021-02-13 14:57:47 -0600

    commit tree 27529513a102c0884407ba0498ed85183f5ce65e

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+
+require "pry"
+puts "hello"
 def square_array(array)
-  # your code here
+  array_2 = []
+  array.each do |number|
+    array_2 << number ** 2 #shovel in new array, #sqaure each number
+  end
+  array_2 #return new array
 end
+
+
